aaeb57f23242e95370d59ad3a65403598b7b46e0
dxliang001 <dongxinliang6@gmail.com>
2023-06-08 10:55:43 -0400

    set up logger

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,22 +5,33 @@ import services.ClientService;
 
 import java.util.List;
 
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.LogManager;
+
 public class Main {
 
+    private static final Logger logger = LogManager.getLogger(Main.class);
+
     public static void main(String[] args) {
+        ConnectionPool connectionPool = null;
+        try {
+            connectionPool = new ConnectionPool();
 
-        ConnectionPool connectionPool = new ConnectionPool();
+            // Create the DAO and Service instances
+            ClientsDAOJDBC clientsDAO = new ClientsDAOJDBC(connectionPool);
+            ClientService clientService = new ClientService(clientsDAO);
 
-        // Create the DAO and Service instances
-        ClientsDAOJDBC clientsDAO = new ClientsDAOJDBC(connectionPool);
-        ClientService clientService = new ClientService(clientsDAO);
-
-        // Fetch a client by ID
-        List<Clients> allClients = clientService.getAllClients();
-        for (Clients client : allClients) {
-            System.out.println("Client: " + client);
+            // Fetch a client by ID
+            List<Clients> allClients = clientService.getAllClients();
+            for (Clients client : allClients) {
+                logger.info("Client: " + client);
+            }
+        } catch (Exception e) {
+            logger.error("An error occurred.", e);
+        } finally {
+            if (connectionPool != null) {
+                ConnectionPool.shutdown();
+            }
         }
-
-        ConnectionPool.shutdown();
     }
 }
