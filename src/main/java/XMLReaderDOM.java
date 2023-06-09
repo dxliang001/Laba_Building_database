b3de3579c1de030e30ed70d52e3aa3d41e74f6fa
dxliang001 <dongxinliang6@gmail.com>
2023-06-09 01:16:31 -0400

    Parse XML TO DOM

---
+++ src/main/java/XMLReaderDOM.java
@@ -0,0 +1,36 @@
+import org.w3c.dom.*;
+import javax.xml.parsers.*;
+import java.io.File;
+
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.LogManager;
+public class XMLReaderDOM {
+
+    private static final Logger logger = LogManager.getLogger(Main.class);
+
+
+    public static void main(String[] args) {
+        try {
+            File xmlFile = new File("src/main/resources/xml/company.xml");
+            DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder builder = factory.newDocumentBuilder();
+            Document document = builder.parse(xmlFile);
+
+            document.getDocumentElement().normalize();
+
+
+            NodeList employees = document.getElementsByTagName("employee");
+            for (int i = 0; i < employees.getLength(); i++) {
+                Node employeeNode = employees.item(i);
+                if (employeeNode.getNodeType() == Node.ELEMENT_NODE) {
+                    Element employeeElement = (Element) employeeNode;
+                    logger.info("Employee ID: " + employeeElement.getElementsByTagName("emp_id").item(0).getTextContent());
+
+                }
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
