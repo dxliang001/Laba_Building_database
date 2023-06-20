3c953d77efc5dfa0b5c1a80cbd8dbddc47a6c503
dxliang001 <dongxinliang6@gmail.com>
2023-06-20 02:11:28 -0400

    Finish up clientMapper

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,11 +5,15 @@ import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import java.io.IOException;
 import java.io.Reader;
+import java.util.List;
 
 public class Main {
 
+    private static final Logger logger = LogManager.getLogger(Main.class);
     public static void main(String[] args) {
         SqlSession session = null;
         try {
@@ -33,12 +37,22 @@ public class Main {
 
             // Test getClientById
             Clients client = mapper.getClientById(2);
-            System.out.println("Client " + client.getClientName());
+            logger.info("Client " + client.getClientName());
 
 
             client = mapper.getClientById(5);
-            System.out.println("Client : " + client);
+            logger.info("Client : " + client);
 
+            List<Clients> allClients = mapper.getAllClients();
+            for (Clients clients : allClients) {
+                logger.info("Client ID: " + clients.getClientId());
+                logger.info("Client Name: " + clients.getClientName());
+                logger.info("Contact Name: " + clients.getContactName());
+                logger.info("Client Address: " + clients.getClientAddress());
+                logger.info("Client Email: " + clients.getClientEmail());
+                logger.info("Client Phone: " + clients.getClientPhone());
+                logger.info("------------------------");
+            }
 
         } catch (IOException e) {
             e.printStackTrace();
